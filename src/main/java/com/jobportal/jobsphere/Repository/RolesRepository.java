aee10002a3e8c1c8291864d3b8bbafce5ace61c7
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-05 19:57:41 +0530

    Job Sphere Project Files Are Create

--- src/main/java/com/jobportal/jobsphere/Repository/RolesRepository.java
+++ src/main/java/com/jobportal/jobsphere/Repository/RolesRepository.java
@@ -1,5 +1,6 @@
 package com.jobportal.jobsphere.Repository;
 
+import com.jobportal.jobsphere.Entity.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigInteger;
