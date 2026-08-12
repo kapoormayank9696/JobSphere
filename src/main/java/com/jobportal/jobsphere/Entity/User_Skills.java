21df6905f49a9507bb37b0242e40f58682de6ebe
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-12 13:49:28 +0530

    Job Sphere Project Files Are Create

--- src/main/java/com/jobportal/jobsphere/Entity/User_Skills.java
+++ src/main/java/com/jobportal/jobsphere/Entity/User_Skills.java
@@ -1,4 +1,32 @@
 package com.jobportal.jobsphere.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+import java.time.Year;
+
+@Entity
+@Table(name = "skills")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User_Skills {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id",nullable = false)
+    private BigInteger userId;
+
+    @Column(name = "skill_id",nullable = false)
+    private BigInteger skillId;
+
+    @Column(name = "proficiency_level",nullable = false)
+    private String proficiencyLevel;
+
+    @Column(name = "years_of_experience",nullable = false)
+    private Year yearOfExperience;
 }
