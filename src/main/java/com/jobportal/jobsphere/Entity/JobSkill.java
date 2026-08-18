f06bc12ecf0e85d6d0274a2cca9e7e508a18537e
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-18 20:07:00 +0530

    Job Sphere Project Files Are Create

---
+++ src/main/java/com/jobportal/jobsphere/Entity/JobSkill.java
@@ -0,0 +1,23 @@
+package com.jobportal.jobsphere.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "jobs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class JobSkill {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_id")
+    private Jobs job;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id")
+    private Skill skill;
+}
