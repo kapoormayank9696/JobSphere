bba151a8feca4dd59d2dba72084c92bc24b09cd0
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-13 12:42:54 +0530

    Job Sphere Project Files Are Create

--- src/main/java/com/jobportal/jobsphere/dto/user/UserUpdateResponse.java
+++ src/main/java/com/jobportal/jobsphere/dto/user/UserUpdateResponse.java
@@ -1,4 +1,26 @@
 package com.jobportal.jobsphere.dto.user;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserUpdateResponse {
+
+    @Size (
+            min = 2,
+            max = 255,
+            message = "Full name must be between 2 and 255 characters"
+    )
+    private String fullName;
+
+    @Pattern(
+            regexp = "^[0-9]{10}$",
+            message = "Phone number must contain exactly 10 digits"
+    )
+    private String phone;
 }
