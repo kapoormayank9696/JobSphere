fd96710a0d92f67786c793b9ac8a6c93e3754f6d
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-19 18:57:42 +0530

    Job Sphere Project Files Are Create

--- src/main/java/com/jobportal/jobsphere/dto/job/JobCreateRequest.java
+++ src/main/java/com/jobportal/jobsphere/dto/job/JobCreateRequest.java
@@ -1,5 +1,6 @@
 package com.jobportal.jobsphere.dto.job;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -7,12 +8,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class JobCreateRequest {
+
+    @NotBlank(message = "Company Id is required")
+    private Integer userId;
 
     @NotBlank(message = "Tile is must be required")
     @Size (
@@ -34,4 +40,21 @@ public class JobCreateRequest {
             message = "Requirement cannot be exceed from 5000 characters"
     )
     private String requirement;
+
+    @NotBlank(message = "Employee Type is required")
+    @Size (
+            max = 50,
+            message = "Employee type is cannot exceed to 50 characters"
+    )
+    private String employeeType;
+
+    @NotBlank(message = "Experience Level is required")
+    @Size (
+            max = 50,
+            message = "Experience level is cannot exceed to 50 characters"
+    )
+    private String experienceLevel;
+
+    @NotBlank(message = "Minimum Salary is required")
+    private BigDecimal salaryMin;
 }
--- src/main/java/com/jobportal/jobsphere/dto/job/JobResponse.java
+++ src/main/java/com/jobportal/jobsphere/dto/job/JobResponse.java
@@ -1,4 +1,25 @@
 package com.jobportal.jobsphere.dto.job;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+
 public class JobResponse {
+
+    private Integer id;
+    private Integer userId;
+    private String title;
+    private String description;
+    private String requirement;
+    private String employeeType;
+    private String experienceLevel;
+    private BigDecimal salaryMin;
+    private BigDecimal salaryMax;
+    private String location;
+    private String city;
+
 }
