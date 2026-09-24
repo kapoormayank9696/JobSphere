db983496f0d699d2d2e1b238fca5acf89c65666f
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-24 11:41:43 +0530

    Job Sphere Project Files Are Create

--- src/main/java/com/jobportal/jobsphere/Controller/UserSkillController.java
+++ src/main/java/com/jobportal/jobsphere/Controller/UserSkillController.java
@@ -1,4 +1,25 @@
 package com.jobportal.jobsphere.Controller;
 
+import com.jobportal.jobsphere.service.UserSkillService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+
 public class UserSkillController {
+    private final UserSkillService userSkillService;
+
+    public UserSkillController(UserSkillService userSkillService) {
+        this.userSkillService = userSkillService;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable  Long userId, Long skillId) {
+        userSkillService.deleteUserSkill(userId,skillId);
+        return ResponseEntity.noContent().build();
+    }
 }
