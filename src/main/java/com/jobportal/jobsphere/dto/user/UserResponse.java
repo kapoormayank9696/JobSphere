ae7018c6106cddadcf40e8e3e39781efd20729b5
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-25 21:24:39 +0530

    Job Sphere Project Files Are Create

--- src/main/java/com/jobportal/jobsphere/dto/user/UserResponse.java
+++ src/main/java/com/jobportal/jobsphere/dto/user/UserResponse.java
@@ -2,8 +2,12 @@ package com.jobportal.jobsphere.dto.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 
 public class UserResponse {
